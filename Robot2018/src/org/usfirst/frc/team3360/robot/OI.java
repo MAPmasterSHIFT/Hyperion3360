8ae4aba7a8284d1e63ad35878caaf6dcc0f5472f
Unknown <zachariebouchard30@hotmail.com>
2018-01-23 21:58:15 -0500

    Clean OI

--- Robot2018/src/org/usfirst/frc/team3360/robot/OI.java
+++ Robot2018/src/org/usfirst/frc/team3360/robot/OI.java
@@ -7,11 +7,7 @@
 
 package org.usfirst.frc.team3360.robot;
 
-
-
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
-
 
 /**
  * This class is the glue that binds the controls on the physical operator
@@ -22,15 +18,11 @@ public class OI {
 	public Joystick LJoystick;
 	public Joystick CoPilotJoystick;
 	
-	public JoystickButton ButtonSideJoystick;
-	
 	public OI(){
 		RJoystick = new Joystick(0);
 		LJoystick = new Joystick(1);
 		CoPilotJoystick = new Joystick(2);
 		
-		//ButtonSideJoystick = new JoystickButton(CoPilotJoystick, 12) ;
-		//ButtonSideJoystick.whenReleased(Robot.elevator.stopFreeMode());
 	}
 	
 	public Joystick getRJoystick() {
